a5ccaa5e06c5271885d89d87d1e932569f0edc9c
MarcusMcCandless <Matanman@mail.ru>
2016-04-27 16:42:58 +0300

    добавлена возможность удаления и добавления избранных видов спорта; Активити называется EditUserSportsActivity;

---
+++ app/src/main/java/com/chokavo/chosportsman/ui/activities/EditUserSportsActivity.java
@@ -0,0 +1,149 @@
+package com.chokavo.chosportsman.ui.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.chokavo.chosportsman.R;
+import com.chokavo.chosportsman.models.DataManager;
+import com.chokavo.chosportsman.network.RFManager;
+import com.chokavo.chosportsman.ormlite.DBHelperFactory;
+import com.chokavo.chosportsman.ormlite.dao.SportsmanFavSportTypeDao;
+import com.chokavo.chosportsman.ormlite.models.SSportType;
+import com.chokavo.chosportsman.ormlite.models.Sportsman;
+import com.chokavo.chosportsman.ui.adapters.CheckableItemAdapter;
+import com.chokavo.chosportsman.ui.adapters.EditUserSportsAdapter;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by Dashitsyren on 27.04.2016.
+ */
+public class EditUserSportsActivity extends BaseActivity {
+    RecyclerView mSportsRecyclerView;
+    EditUserSportsAdapter mSportsAdapter;
+    Button mBtnAddSport;
+
+    List<SSportType> mSportTypes = new ArrayList<>();
+
+    private MaterialDialog mDialogSportType;
+    int mChosenSportKindId = -1;
+    SSportType mChosenSportType;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_sports);
+        Toolbar mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mActionBarToolbar);
+        if (getSupportActionBar()!=null) {
+            getSupportActionBar().setTitle(R.string.favorite_user_sports);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_white_24dp);
+        }
+
+
+        mSportsRecyclerView = (RecyclerView)findViewById(R.id.recview_favorite_sports);
+        mSportTypes = DataManager.getInstance().mSportsman.getFavSportTypes();
+        mSportsAdapter = new EditUserSportsAdapter(mSportTypes, new EditUserSportsAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(SSportType sportType) {
+                //TODO:intent to sport profile
+            }
+        }, new EditUserSportsAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(SSportType sportType) {
+                mSportTypes.remove(sportType);
+                DataManager.getInstance().mSportsman.setFavSportTypes(mSportTypes);
+                mSportsAdapter.notifyDataSetChanged();
+            }
+        });
+        mSportsRecyclerView.setAdapter(mSportsAdapter);
+        mSportsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mBtnAddSport = (Button) findViewById(R.id.btn_add_sporttype);
+        mBtnAddSport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialogSportKind();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showDialogSportKind() {
+        final CharSequence[] mSportTypesChars = DataManager.getInstance().getSportTypesAsChars();
+        mDialogSportType = new MaterialDialog.Builder(this)
+                .adapter(new CheckableItemAdapter(this, mSportTypesChars, mChosenSportKindId),
+                        new MaterialDialog.ListCallback() {
+                            @Override
+                            public void onSelection(MaterialDialog dialog, View itemView, int which, CharSequence text) {
+                                mChosenSportKindId = which;
+                                mChosenSportType = DataManager.getInstance().getSportTypeByName(mSportTypesChars[which]);
+                                mSportTypes.add(mChosenSportType);
+                                DataManager.getInstance().mSportsman.setFavSportTypes(mSportTypes);
+                                mSportsAdapter.notifyDataSetChanged();
+                                dialog.cancel();
+                            }
+                        })
+                .build();
+        mDialogSportType.show();
+        mChosenSportKindId = -1;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        setSportTypesOnServer();
+    }
+
+    private void setSportTypesOnServer() {
+        RFManager.setUserSportTypes(DataManager.getInstance().mSportsman.getServerId(),
+                mSportTypes, new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        Log.e(ChooseSportsActivity.class.getSimpleName(), "onResponse");
+                        // TODO save in SQLite
+//                        saveFavSportsSQLite(DataManager.getInstance().mSportsman,
+//                                mSportTypes);
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        Log.e(ChooseSportsActivity.class.getSimpleName(), "onFailure: " + t);
+                    }
+                });
+    }
+
+    private void saveFavSportsSQLite(Sportsman sportsman, List<SSportType> favSportTypes) {
+        try {
+            SportsmanFavSportTypeDao dao = DBHelperFactory.getHelper().getSportsmanFavSportTypeDao();
+            dao.createListIfNotExist(sportsman, favSportTypes);
+            DataManager.getInstance().mSportsman.setFavSportTypes(favSportTypes);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/chokavo/chosportsman/ui/adapters/EditUserSportsAdapter.java
@@ -0,0 +1,86 @@
+package com.chokavo.chosportsman.ui.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.chauthai.swipereveallayout.SwipeRevealLayout;
+import com.chauthai.swipereveallayout.ViewBinderHelper;
+import com.chokavo.chosportsman.R;
+import com.chokavo.chosportsman.ormlite.models.SSportType;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Dashitsyren on 27.04.2016.
+ */
+public class EditUserSportsAdapter extends RecyclerView.Adapter<EditUserSportsAdapter.ViewHolder> {
+
+    public interface OnItemClickListener {
+        void onItemClick(SSportType sportType);
+    }
+
+    private List<SSportType> mUserSports = new ArrayList<>();
+    private final OnItemClickListener mItemListener;
+    private final OnItemClickListener mDeleteListener;
+    private final ViewBinderHelper viewBinderHelper = new ViewBinderHelper();
+
+    public EditUserSportsAdapter(List<SSportType> sportTypes, OnItemClickListener listener, OnItemClickListener deleteListener) {
+        mUserSports = sportTypes;
+        mItemListener = listener;
+        mDeleteListener = deleteListener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_edit_user_sports, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.bind(mUserSports.get(position), mItemListener, mDeleteListener);
+        viewBinderHelper.bind(holder.mSwipeRevealLayout, Integer.toString(mUserSports.get(position).getId()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUserSports == null ? 0 : mUserSports.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView mTxtSportType;
+        public ImageView mImgSport, mImgHaveProfile;
+        public SwipeRevealLayout mSwipeRevealLayout;
+        public FrameLayout mFrameDeleteSport;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTxtSportType = (TextView) itemView.findViewById(R.id.txt_sporttype);
+            mImgSport = (ImageView) itemView.findViewById(R.id.image_sport);
+            mImgHaveProfile = (ImageView) itemView.findViewById(R.id.img_have_sport_profile);
+            mSwipeRevealLayout = (SwipeRevealLayout) itemView.findViewById(R.id.swipe_layout);
+            mFrameDeleteSport = (FrameLayout) itemView.findViewById(R.id.frame_delete_user_sport);
+        }
+
+        public void bind(final SSportType item, final OnItemClickListener listener, final OnItemClickListener deleteListener) {
+            mTxtSportType.setText(item.getTitle());
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(item);
+                }
+            });
+            mFrameDeleteSport.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    deleteListener.onItemClick(item);
+                }
+            });
+        }
+    }
+}
